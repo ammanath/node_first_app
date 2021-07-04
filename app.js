c7c74d277532b7c5abb45eb5484c7089a8618691
brijesh <github@ammanath.com>
2021-07-04 21:08:43 +0530

    event args

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 
+const loger =  require('./logger');
+
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 
 //Register a listener
-emitter.on('messageLogged', function(arg){
+emitter.on('messageLogged', (arg)=> {
     console.log('Listener called', arg);
 })
 
 //Rasie an event
 emitter.emit('messageLogged',{id:1,url:'url'});
+
+loger.log('Log message 940');
--- logger.js
+++ logger.js
@@ -1,9 +1,15 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
 
 var url = 'http://mylogger.io/log';
+
+emitter.on('logging', arg=>console.log('Logging called', arg));
 
 function log(message){
     //Send http request to log
     console.log(message);
+    emitter.emit('logging', {data:message});
 }
 
 
