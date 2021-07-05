e6c2e50f1beecf567c94e6f20308182bf8ccd4eb
brijesh <github@ammanath.com>
2021-07-05 21:30:24 +0530

    http req res

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const http = require('http');
 const EventEmitter = require('events');
 const { Socket } = require('dgram');
 
-const server = http.createServer();
-server.on('connection', (socket)=>{
-    console.log('New connection');
+const server = http.createServer((req, res)=>{
+    if(req.url==='/'){
+        res.write('Hello world');
+        res.end();
+    }
+
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3,55]));
+        res.end();
+    }
 });
+
 server.listen(3000);
 
 console.log('Listening on port 3000...');
