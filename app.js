1ece7b6f2e323574a247819b45dd22bb1a47948c
brijesh <github@ammanath.com>
2021-07-05 21:24:47 +0530

    http

--- app.js
+++ app.js
@@ -2,8 +2,18 @@
 const Logger =  require('./logger');
 const logger = new Logger();
 
-const EventEmitter = require('events');
+const http = require('http');
 
+const EventEmitter = require('events');
+const { Socket } = require('dgram');
+
+const server = http.createServer();
+server.on('connection', (socket)=>{
+    console.log('New connection');
+});
+server.listen(3000);
+
+console.log('Listening on port 3000...');
 
 //Register a listener
 logger.on('logging', (arg)=> {
